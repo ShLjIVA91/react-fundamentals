cfcc6d5e0bf913da94cee439d84609cd9e45340f
Katarina Kaštevarac <katarina.kashtevarac@gmail.com>
2023-04-25 11:55:16 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+import Navbar from './assets/Navbar';
+import Hero from './assets/Hero';
+import About from './assets/About';
+import Services from './assets/Services';
+import Tours from './assets/Tours';
+import Footer from './assets/Footer';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
